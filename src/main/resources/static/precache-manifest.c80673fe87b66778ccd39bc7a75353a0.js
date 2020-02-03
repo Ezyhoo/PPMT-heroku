bc67e3a4ddb3da8813b79bfcd1da59b076e45150
Ezyhoo <512470516@qq.com>
2020-02-02 23:35:07 -0600

    trying

---
+++ src/main/resources/static/precache-manifest.c80673fe87b66778ccd39bc7a75353a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93be9c0de20ad362ea45699f3a0a7a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68bb9d198d0d5b67d4e1",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f91fe1c4f5e92c666f8a",
+    "url": "/static/css/main.b1c627ca.chunk.css"
+  },
+  {
+    "revision": "68bb9d198d0d5b67d4e1",
+    "url": "/static/js/2.46ed4632.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.46ed4632.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f91fe1c4f5e92c666f8a",
+    "url": "/static/js/main.af4831c6.chunk.js"
+  },
+  {
+    "revision": "b47d682f800ac873db0c",
+    "url": "/static/js/runtime-main.04b7d0ad.js"
+  }
+]);
