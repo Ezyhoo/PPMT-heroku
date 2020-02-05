0de7e3d01e3bf02c36b11edfb796138d5016ac9a
Ezyhoo <512470516@qq.com>
2020-02-04 22:45:02 -0600

    heroku backend

---
+++ src/main/resources/static/precache-manifest.caa974582aa0c0894e1e7c16af4fc9c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a619fc04770020d03e5403ac141b2845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68bb9d198d0d5b67d4e1",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "41f7f7d5db38a836a1be",
+    "url": "/static/css/main.b1c627ca.chunk.css"
+  },
+  {
+    "revision": "68bb9d198d0d5b67d4e1",
+    "url": "/static/js/2.46ed4632.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.46ed4632.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41f7f7d5db38a836a1be",
+    "url": "/static/js/main.32602645.chunk.js"
+  },
+  {
+    "revision": "b47d682f800ac873db0c",
+    "url": "/static/js/runtime-main.04b7d0ad.js"
+  }
+]);
